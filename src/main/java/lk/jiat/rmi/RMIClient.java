360259ded6fad6084e685b1400c88b1c67df64d3
Sachith Gunarathna <106460379+Sachith-Gunarathna@users.noreply.github.com>
2026-05-20 12:03:22 +0530

    v1.0.2

--- src/main/java/lk/jiat/rmi/RMIClient.java
+++ src/main/java/lk/jiat/rmi/RMIClient.java
@@ -1,8 +1,23 @@
 package lk.jiat.rmi;
 
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 public class RMIClient {
 
     public static void main(String[] args) {
+        try {
+
+            Registry registry = LocateRegistry.getRegistry("localhost",6666);
+
+            String[] list = registry.list();
+            for (String s : list){
+                System.out.println(s);
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
